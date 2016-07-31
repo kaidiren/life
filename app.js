768290a0c297fe62d9b4b307dfd97eb90a1811a0
kaidiren <kaidiren@gmail.com>
2016-07-31 21:25:57 +0800

    update

--- app.js
+++ app.js
@@ -1,32 +1,28 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function(req, res) {
-  res.send('Hello https!');
-});
-
-const LEX = require('letsencrypt-express').testing();
-
-const DOMAIN = 'www.xli.me';
+const LEX = require('letsencrypt-express');
+const DOMAIN = 'xli.me,www.xli.me';
 const EMAIL = 'kaidiren@gmail.com';
 
 const lex = LEX.create({
   configDir: require('os').homedir() + '/life',
   approveRegistration: function(hostname, approve) {
-    if (hostname === DOMAIN) {
-      approve(null, {
-        domains: [DOMAIN],
-        email: EMAIL,
-        agreeTos: true,
-        server: LEX.productionServerUrl
-      });
-    }
+    approve(null, {
+      domains: [DOMAIN],
+      email: EMAIL,
+      agreeTos: true,
+      server: LEX.productionServerUrl
+    });
   }
+});
+
+app.get('/', function(req, res) {
+  res.send('Hello https!');
 });
 
 
 lex.onRequest = app;
-
 lex.listen([80], [443, 5001], function() {
   const protocol = ('requestCert' in this) ? 'https' : 'http';
   console.log("Listening at " + protocol + '://127.0.0.1:' + this.address().port);
