d1ef9b86b215c01d691cf6fd0609c13c3216c581
kaidiren <kaidiren@gmail.com>
2016-07-31 20:58:35 +0800

    update

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  res.send('Hello https!');
 });
 
 const LEX = require('letsencrypt-express').testing();
 
-const DOMAIN = 'www.xli.me';
+const DOMAIN = 'xli.me,www.xli.me';
 const EMAIL = 'kaidiren@gmail.com';
 
 const lex = LEX.create({
